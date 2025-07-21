46924dd78827d7e4306218c1978b58a79c97dfef
rahul54726 <vermarahul11034@gmail.com>
2025-07-21 18:16:43 +0530

    Leetcode 1957

--- LeetCode/Easy/FizzBuzz.java
+++ LeetCode/Easy/FizzBuzz.java
@@ -22,8 +22,24 @@ public class FizzBuzz {
         }
        return result;
     }
-
+    public static String makeFancyString(String s) {
+        StringBuilder sb = new StringBuilder();
+        int freq = 1;
+        for(int i = 1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1)){
+                freq++;
+            }
+            else {
+                freq = 1;
+            }
+            if(freq <= 2){
+                sb.append(s.charAt(i));
+            }
+        }
+        return s.isEmpty() ? "" : s.charAt(0) + sb.toString();
+    }
     public static void main(String[] args) {
-        System.out.println(fizzBuzz(30));
+//        System.out.println(fizzBuzz(30));
+        System.out.println(makeFancyString(" "));
     }
 }
