7356cb1d113ba8d34b061538c2bc8c09d81c71bb
rahul54726 <vermarahul11034@gmail.com>
2025-07-22 12:20:42 +0530

    SOmthing New

--- Streams/IntermediateOps.java
+++ Streams/IntermediateOps.java
@@ -10,7 +10,7 @@ public class IntermediateOps {
         //Intermediate ops transform a stream into another stream
         //They are lazy , means they don't execute until terminal Operation is Invoked.
         //1.filter
-        List<String> names = Arrays.asList("Rahul","Kunal","Vipul","Rajul");
+        List<String> names = Arrays.asList("Rahul","Kunal","Vipul","Rajul","a","Rahul");
         Stream<String> filteredStream = names.stream().filter(name -> name.startsWith("R"));
         //no filtering at this point
         List<String> collect = filteredStream.collect(Collectors.toList());
@@ -19,8 +19,11 @@ public class IntermediateOps {
         List<String> collect1 = upperStream.collect(Collectors.toList());
 //        3.sorted
         Stream<String> sorted = names.stream().sorted();
-        Stream<String> sortedStreamUsingComparator = names.stream().sorted((a,b) -> (a.length() - b.length()));
-
-
+        List<String> sortedStreamUsingComparator = names.stream().sorted((a, b) -> (a.length() - b.length())).collect(Collectors.toList());
+        System.out.println(sortedStreamUsingComparator);
+//        4.Distinct
+        long count = names.stream().filter(name -> name.startsWith("R")).distinct().count();
+        System.out.println(count);
+        System.out.println(Stream.iterate(1,x -> x  +1 ).skip(10).limit(100).toList());
     }
 }
