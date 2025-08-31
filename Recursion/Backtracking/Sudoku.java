919dfb0bc4b632ec469103336cc3a9fafd1e422a
rahul54726 <vermarahul11034@gmail.com>
2025-08-31 09:33:05 +0530

    Revision of BackTracking

--- Recursion/Backtracking/Sudoku.java
+++ Recursion/Backtracking/Sudoku.java
@@ -47,6 +47,27 @@ public class Sudoku {
         }
         return true;
     }
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
+
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
+                        return false;
+                    }
+
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
+                }
+            }
+        }
+        return true;
+    }
     static boolean solve(int[][] board){
 //        this is how we are replacing r,c from arguments
         int n=board.length;
